5de73ae2bdf006d59c6a1e218ae79528e589eae7
Stakshi <128694806+Stakshi301@users.noreply.github.com>
2024-11-26 18:51:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	const form=document.getElementById('form1');
+	const Fname=form.elements['fname'].value;
+	const Lname=form.elements['lname'].value;
+  alert(`${Fname} ${Lname}`);
 }
